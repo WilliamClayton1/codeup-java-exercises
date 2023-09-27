0557beeb119dc39d87b48b3703ac36b352a2b91a
William Clayton <william.d.clayton1@gmail.com>
2023-09-27 14:43:29 -0500

    -wc

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,36 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World");
+        System.out.println("Hello, World");
+
+        int myFavoriteNumber = 49;
+
+        String myString = "Hello, World!";
+
+
+        long myNumber;
+
+        myNumber = 123L;
+
+        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        System.out.println(++x);
+        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+
+        x = 4;
+
+        x += 5;
+
+        x = 3;
+
+        int y = 4;
+
+        y *= x;
     }
 }
