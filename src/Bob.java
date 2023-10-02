6f600d6c4da4d3933944a9abb5a64d53199b0512
William Clayton <william.d.clayton1@gmail.com>
2023-10-02 14:32:28 -0500

    -wc

---
+++ src/Bob.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean conversation = false;
+
+        do {
+            System.out.println("What's Up?");
+
+            String userInput = scanner.nextLine();
+
+            if (userInput.lastIndexOf("?") != -1) {
+                System.out.println("Sure.");
+            } else if (userInput.lastIndexOf("!") != -1) {
+                System.out.println("Whoa, chill out!");
+            } else if (userInput.lastIndexOf("") != -1) {
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever.' to anything else");
+            }
+
+            System.out.println("Do you wanna keep talking? (Yes/No)");
+            scanner.nextLine();
+            String userAnswer = scanner.nextLine();
+
+            conversation = userAnswer.equalsIgnoreCase("yes");
+        }
+        while (conversation);
+
+    }
+}
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,17 +1,17 @@
 public class StringExercise {
     public static void main (String[] args) {
 
-        String string1 = "We don't need no education";
+        String string1 = "We don't need no education\n" +
+                "We don't need no thought control";
         System.out.println(string1);
-        String string2 = "We don't need no education";
+        String string2 = "Check \"this\" out!, \"s inside of \"s!";
         System.out.println(string2);
-        String string3 = "Check \"this\" out!, \"s inside of \"s!";
+        String string3 = "In windows, the main drive is usually C:\\";
         System.out.println(string3);
-        String string4 = "In windows, the main drive is usually C:\\";
-        System.out.println(string4);
-        String string5 = "I can do backslashes \\, double backslashes \\\\,\n" +
+        String string4 = "I can do backslashes \\, double backslashes \\\\,\n" +
                 "and the amazing triple backslash \\\\\\!";
-        System.out.println(string5);
+        System.out.println(string4);
 
     }
+
 }
