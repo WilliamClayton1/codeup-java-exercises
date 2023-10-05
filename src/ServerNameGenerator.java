3e39d489f06f2e231b4843fe9998bca73c49808b
William Clayton <william.d.clayton1@gmail.com>
2023-10-05 11:13:19 -0500

    -wc

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,18 @@
+public class ServerNameGenerator {
+
+    public static int getRandom(int max){ return (int) (Math.random()*max); }
+
+    public static String getRandomElement(String[] elementArray){
+        int randomNum = getRandom(10);
+        return elementArray[randomNum];
+    }
+
+    public static void main (String[] args){
+        String[] adjective = {"Ashamed", "Adorable", "Attractive", "Beautiful", "Awful", "Aggressive", "Cruel", "Clever", "Tasty", "Jealous"};
+        String[] noun = {"man", "woman", "teacher", "dog", "cat", "home", "office", "town", "countryside", "America"};
+
+        String randomAdj = getRandomElement(adjective);
+        String randomNoun = getRandomElement(noun);
+        System.out.println(randomAdj + "-" + randomNoun);
+    }
+}
